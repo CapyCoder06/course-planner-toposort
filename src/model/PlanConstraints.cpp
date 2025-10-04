803d57183a2262b5ef1a8052836ca5401ba3fd43
Penguin <putin.8306@gmail.com>
2025-10-04 17:46:58 +0700

    C2

--- src/model/PlanConstraints.cpp
+++ src/model/PlanConstraints.cpp
@@ -1,26 +1,24 @@
 #include "PlanConstraints.h"
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
-void PlanConstraints::validate() const
-{
-    if (mincredits<=0)
-    {
-        throw runtime_error("Tín chỉ tối thiểu phải lớn hơn 0");
-    }
-    if (maxcredits<mincredits)
-    {
-        throw runtime_error("Tín chỉ tối đa phải lớn hơn hoặc bằng tín chỉ tối thiểu");
-    }
-    if (numTerms <=0)
-    {
-        throw runtime_error("Số kì phải lớn hơn 0");
-    }
-    for (int t : offered_terms)
-    {
-        if (t<1 || t>numTerms)
-        {
-            throw runtime_error("Số kì phải nằm trong khoảng từ 1 đến" + to_string(numTerms));
+void PlanConstraints::validate() const {
+    if (minCreditsPerTerm <= 0)
+    throw invalid_argument("Số tín chỉ tối thiểu (" + to_string(minCreditsPerTerm) + ") phải > 0");
+
+    if (maxCreditsPerTerm < minCreditsPerTerm)
+    throw invalid_argument("Tối đa (" + to_string(maxCreditsPerTerm) + 
+                           ") nhỏ hơn tối thiểu (" + to_string(minCreditsPerTerm) + ")");
+
+    if (numTerms <= 0)
+    throw invalid_argument("Số học kỳ (" + to_string(numTerms) + ") phải > 0");
+
+    for (int t : offered_terms) {
+        if (t < 1 || t > numTerms) {
+            throw invalid_argument("Học kỳ " + to_string(t) + 
+                                " không hợp lệ, cho phép [1.." + to_string(numTerms) + "]");
         }
     }
 }
